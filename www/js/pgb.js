6264cb64c6c97e4755f2ceb7fac65d1da3970323
4kiru <Mateusz.salawa@gmail.com>
2017-03-07 13:10:47 +0100

    new commit upload

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,27 +6,13 @@ function onDeviceReady() {
 	navigator.notification.beep(1);
 }
 
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
-
-function mydata()
+function mycarddata()
 {
 	
 	datainfo = 'Your Personal Data : ' + '\n' +
-	'Name : mateusz'+
-	'Nazwisko : Salawa' +
+	'Name : mateusz' + '\n' +
+	'Nazwisko : Salawa' + '\n' +
 	'Student : Wsei';
+	
 	navigator.notification.alert(datainfo);
 }
